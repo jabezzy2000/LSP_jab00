f443f75b4997b871c50a733d711b0eb8cd57ce2f
jabezzy2000 <jabez.agyemang-prem@bison.howard.edu>
2023-02-02 14:22:06 -0500

    Edited output method

--- lsp_spring_23/src/org/howard/edu/hw2/WordCounting.java
+++ lsp_spring_23/src/org/howard/edu/hw2/WordCounting.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class WordCounting {
 
@@ -48,7 +49,9 @@ public class WordCounting {
             }
         }
         
-        System.out.println(wordCount);
+        for (Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+          }
         
 
 	}
